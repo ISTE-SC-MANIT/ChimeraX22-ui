3af0be334d48a7848d459eb1cdf88aa4da4b86df
22tushar <gehlottushar18@gmal.com>
2021-12-28 13:32:02 +0530

    fix : classes issue

---
+++ src/components/theme/index.ts
@@ -0,0 +1,77 @@
+import {
+  ThemeOptions,
+  SimplePaletteColorOptions,
+  Theme,
+  createTheme 
+} from "@mui/material/styles";
+import { blue, orange } from "@mui/material/colors";
+import React from "react";
+export const themeProps: ThemeOptions['props'] = {
+  MuiTextField: {
+    variant: 'outlined',
+    margin: 'dense',
+  },
+};
+
+export const defaultPrimary = blue[500];
+export const defaultSecondary = orange[500];
+export const defaultMode = "light";
+export interface ThemeContext {
+  mode: ThemeOptions["palette"]["type"];
+  primary: SimplePaletteColorOptions["main"];
+  secondary: SimplePaletteColorOptions["main"];
+  toggleMode: () => void;
+  updateColors: (
+    primary?: SimplePaletteColorOptions["main"],
+    secondary?: SimplePaletteColorOptions["main"]
+  ) => void;
+}
+
+export const themeContext = React.createContext<ThemeContext>({
+  mode: "light",
+  primary: blue[500],
+  secondary: blue[500],
+  toggleMode: () => {
+    /* Do nothing */
+  },
+  updateColors: () => {
+    /* Do nothing */
+  },
+});
+
+export function toggleMode(
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>
+): void {
+  setTheme((oldTheme) => {
+    return createTheme({
+      props: themeProps,
+      palette: {
+        primary: {
+          main: oldTheme.palette.primary.main,
+        },
+        secondary: {
+          main: oldTheme.palette.secondary.main,
+        },
+        type: oldTheme.palette.type === 'dark' ? 'light' : 'dark',
+      },
+      typography: {
+        fontFamily: [
+          'Nunito',
+          'Montserrat',
+          'Roboto',
+          'sans-serif',
+          'Arial',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ].join(','),
+      },
+    });
+  });
+  return;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { createStyles, makeStyles, Theme, useTheme, withStyles } from '@mui/material/styles';
+import { createStyles, Theme, useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { withStyles, makeStyles } from '@mui/styles';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Box, Fade, Grid, Link, useMediaQuery } from '@mui/material';
+import { Box, Fade, Link, Grid , useMediaQuery } from '@mui/material';
 import Image from 'next/image';
 import NavbarHeader from '../components/navbarheader';
 import ThemeToggleButton from '../components/theme/modeToggle';
@@ -56,7 +57,7 @@ const useStyles = makeStyles((theme: Theme) =>
       minHeight: '80vh',
     },
     typo: {
-      color: theme.palette.type === 'light' ? '#1976D2' : 'white',
+      color: theme.palette.mode === 'light' ? '#1976D2' : 'white',
       // color: '#221C64',
       [theme.breakpoints.up('sm')]: {
         padding: theme.spacing(5),
@@ -111,6 +112,115 @@ const Btn = withStyles((theme) => ({
   },
 }))(Button);
 
+// const useStyles = makeStyles((theme: Theme) => ({
+//   root: (props) => ({
+//     minHeight: '100vh',
+//     margin: 0,
+//     padding: 0,
+//     boxSizing: 'border-box',
+//     flexGrow: 1,
+//     background: `url('/bg.svg')`,
+//     backgroundRepeat: 'no-repeat',
+//     backgroundSize: 'cover',
+//   }),
+
+//   header: (props) => ({
+//     minHeight: '10vh',
+//     alignItems: 'center',
+//     flexWrap: 'wrap',
+//     display: 'flex',
+//   }),
+
+//   logo: (props) => ({
+//     marginRight: 'auto',
+//     [theme.breakpoints.down('sm')]: {
+//       margin: 'auto',
+//     },
+//   }),
+
+//   menuBtn: (props) => ({
+//     backgroundColor: '#3997F5',
+//     color: 'white',
+//     marginRight: theme.spacing(1),
+//     '&:hover': {
+//       backgroundColor: '#1976D2',
+//     },
+//     [theme.breakpoints.down('sm')]: {
+//       display: 'none',
+//     },
+//   }),
+//   darkTheme: (props) => ({
+//     marginRight: theme.spacing(2),
+//     [theme.breakpoints.down('sm')]: {
+//       display: 'none',
+//     },
+//   }),
+
+
+//   body: (props) => ({
+//     minHeight: '80vh',
+//   }),
+
+//   typo: (props) => ({
+//     color: theme.palette.mode === 'light' ? '#1976D2' : 'white',
+//     // color: '#221C64',
+//     [theme.breakpoints.up('sm')]: {
+//       padding: theme.spacing(5),
+//     },
+//   }),
+
+//   prize: (props) => ({
+//     width: '80%',
+//     marginLeft: 'auto',
+//     marginRight: 'auto',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     flexDirection: 'column',
+//   }),
+
+//   prizeSection: (props) => ({
+//     [theme.breakpoints.down('sm')]: {
+//       marginBottom: theme.spacing(6),
+//     },
+//   }),
+
+//   margin: (props) => ({
+//     [theme.breakpoints.down('sm')]: {
+//       marginBottom: theme.spacing(3),
+//     },
+//   }),
+
+//   mobileDrawer: (props) => ({
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     [theme.breakpoints.up('md')]: {
+//       display: 'none',
+//     },
+//   }),
+//   flexColumn:   (props) => ( {
+//           display: 'flex',
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           flexDirection: 'column',
+//         }),
+    
+// }));
+
+// const Btn = withStyles((theme) => ({
+//   root:(props) => ({
+//     color: theme.palette.getContrastText('#3997F5'),
+//     backgroundColor: '#3997F5',
+//     border: '2px solid white',
+//     borderRadius: '25px',
+//     padding: '10px 20px',
+//     '&:hover': {
+//       backgroundColor: '#1976D2',
+//     },
+//   }),
+// }))(Button);
+
 const VectorImg = () => {
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('sm'));
@@ -135,158 +245,159 @@ const VectorImg = () => {
 
 
 const Landing: React.FC = () => {
-    const classes = useStyles();
-    const router = useRouter();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-    const handleDrawerOpen = () => {
-      setOpen(true);
-    };
-    return (
-      <>
-        <NavbarHeader open={open} setOpen={setOpen} />
-        <div className={classes.root} onClick={() => setOpen(false)}>
-          <Box padding={4} className={classes.header}>
-            <Box className={classes.logo}>
-              <img
-                style={{ cursor: 'pointer' }}
-                src={
-                  theme.palette.type === 'light'
-                    ? '/ChimeraX-logo-blue.svg'
-                    : '/ChimeraX-logo-whitebg.svg'
-                }
-                width="256px"
-                alt="logo"
-                onClick={() => router.push('/')}
-              />
-            </Box>
-            <Box display="flex" alignItems="center" justifyContent="center">
-              <Fade in={true}>
-                <Box className={classes.darkTheme}>
-                  <ThemeToggleButton />
-                </Box>
-              </Fade>
-              <Button
-                variant="contained"
-                onClick={() => router.push('/login')}
-                className={classes.menuBtn}
-              >
-                Login
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => router.push('/signin')}
-                className={classes.menuBtn}
-              >
-                Sign up
-              </Button>
-            </Box>
+  const classes = useStyles();
+  const router = useRouter();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  return (
+    <>
+      <NavbarHeader open={open} setOpen={setOpen} />
+      <div className={classes.root} onClick={() => setOpen(false)}>
+        <Box padding={4} className={classes.header}>
+          <Box className={classes.logo}>
+            <Image
+              style={{ cursor: 'pointer' }}
+              src={
+                theme.palette.mode === 'light'
+                  ? '/ChimeraX-logo-blue.svg'
+                  : '/ChimeraX-logo-whitebg.svg'
+              }
+              width="256px"
+              alt="logo"
+              onClick={() => router.push('/')}
+            />
           </Box>
-          <Box className={classes.mobileDrawer}>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={(event) => {
-                event.stopPropagation();
-                handleDrawerOpen();
-              }}
+          <Box display="flex" alignItems="center" justifyContent="center">
+            <Fade in={true}>
+              <Box className={classes.darkTheme}>
+                <ThemeToggleButton />
+              </Box>
+            </Fade>
+            <Button
+              variant="contained"
+              onClick={() => router.push('/login')}
+              className={classes.menuBtn}
             >
-              <MenuIcon fontSize="large" color="primary" />
-            </IconButton>
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => router.push('/signin')}
+              className={classes.menuBtn}
+            >
+              Sign up
+            </Button>
           </Box>
-          <Grid container className={classes.body}>
-            <Grid
-              container
-              item
-              xs={12}
-              md={4}
-              justify="space-around"
-              alignItems="center"
-              direction="column"
-              className={classes.prizeSection}
-            >
-              <Box className={classes.margin}>
-                <Typography variant="h4" align="center" className={classes.typo}>
-                  <b>Central India's largest Quizzing Contest</b>
+        </Box>
+        <Box className={classes.mobileDrawer}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={(event) => {
+              event.stopPropagation();
+              handleDrawerOpen();
+            }}
+          >
+            <MenuIcon fontSize="large" color="primary" />
+          </IconButton>
+        </Box>
+        <Grid container className={classes.body}>
+          <Grid
+            container
+            item
+            xs={12}
+            md={4}
+            justifyContent="space-around"
+            alignItems="center"
+            direction="column"
+            className={classes.prizeSection}
+          >
+            <Box className={classes.margin}>
+              <Typography variant="h4" align="center" className={classes.typo}>
+                <b>Central India's largest Quizzing Contest</b>
+              </Typography>
+            </Box>
+            <Box className={classes.margin}>
+              <Box marginBottom={5}>
+                <Typography variant="h5" align="center">
+                  Prelims
+                  <br />
+                  7th Feb' 2021
                 </Typography>
               </Box>
-              <Box className={classes.margin}>
-                <Box marginBottom={5}>
-                  <Typography variant="h5" align="center">
-                    Prelims
-                    <br />
-                    7th Feb' 2021
-                  </Typography>
-                </Box>
-                <Box className={classes.flexColumn} lineHeight={2}>
-                  <Btn onClick={() => router.push('/signin')}>Register Now</Btn>
-                  <Link
-                    target="_blank"
-                    href="https://drive.google.com/file/d/18fYq_uSXg76WQ4Ov6BTZ4rjJFkwTXThV/view?usp=sharing"
-                  >
-                    How to get registered?
-                  </Link>
-                </Box>
+              <Box className={classes.flexColumn} lineHeight={2}>
+                <Btn onClick={() => router.push('/signin')}>Register Now</Btn>
+                <Link
+                  target="_blank"
+                  href="https://drive.google.com/file/d/18fYq_uSXg76WQ4Ov6BTZ4rjJFkwTXThV/view?usp=sharing"
+                >
+                  How to get registered?
+                </Link>
               </Box>
-              <Box className={classes.prize}>
-                <Box marginBottom={2}>
-                  <Typography variant="h5" align="center">
-                    PRIZES
-                  </Typography>
-                </Box>
-                <Grid container justify="center" alignItems="center">
-                  <Grid item xs={4} container justify="center" alignItems="center">
-                    <Box className={classes.flexColumn}>
-                      <img
-                        src={theme.palette.type === 'light' ? '/first.svg' : '/first-white.svg'}
-                        alt="first"
-                        width="70px"
-                      />
-                      <Typography variant="h6">
-                        <b>₹ 15,000</b>
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={4} container justify="center" alignItems="center">
-                    <Box className={classes.flexColumn}>
-                      <img
-                        src={theme.palette.type === 'light' ? '/second.svg' : '/second-white.svg'}
-                        alt="first"
-                        width="70px"
-                      />
-                      <Typography variant="h6">
-                        <b>₹ 10,000</b>
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={4} container justify="center" alignItems="center">
-                    <Box className={classes.flexColumn}>
-                      <img
-                        src={theme.palette.type === 'light' ? '/third.svg' : '/third-white.svg'}
-                        alt="first"
-                        width="70px"
-                      />
-                      <Typography variant="h6">
-                        <b>₹ 5,000</b>
-                      </Typography>
-                    </Box>
-                  </Grid>
+            </Box>
+            <Box className={classes.prize}>
+              <Box marginBottom={2}>
+                <Typography variant="h5" align="center">
+                  PRIZES
+                </Typography>
+              </Box>
+              <Grid container justifyContent="center" alignItems="center">
+                <Grid item xs={4} container justifyContent="center" alignItems="center">
+                  <Box className={classes.flexColumn}>
+                    <Image
+                      src={theme.palette.mode === 'light' ? '/first.svg' : '/first-white.svg'}
+                      alt="first"
+                      width="70px"
+                    />
+                    <Typography variant="h6">
+                      <b>₹ 15,000</b>
+                    </Typography>
+                  </Box>
                 </Grid>
-                <Box className={classes.margin}>
-                  <Typography variant="subtitle1" align="center">
-                    *Merchandise for City Winners!
-                  </Typography>
-                </Box>
+                <Grid item xs={4} container justifyContent="center" alignItems="center">
+                  <Box className={classes.flexColumn}>
+                    <Image
+                      src={theme.palette.mode=== 'light' ? '/second.svg' : '/second-white.svg'}
+                      alt="first"
+                      width="70px"
+                    />
+                    <Typography variant="h6">
+                      <b>₹ 10,000</b>
+                    </Typography>
+                  </Box>
+                </Grid>
+                <Grid  item xs={4} container justifyContent="center" alignItems="center">
+                  <Box className={classes.flexColumn}>
+                    <Image
+                      src={theme.palette.mode === 'light' ? '/third.svg' : '/third-white.svg'}
+                      alt="first"
+                      width="70px"
+                    />
+                    <Typography variant="h6">
+                      <b>₹ 5,000</b>
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Box className={classes.margin}>
+                <Typography variant="subtitle1" align="center">
+                  *Merchandise for City Winners!
+                </Typography>
               </Box>
-            </Grid>
-            <Grid container item xs={12} md={8} justify="flex-end" alignItems="flex-end">
-              <VectorImg />
-            </Grid>
+            </Box>
           </Grid>
-        </div>
-      </>
-    );
+          <Grid container item xs={12} md={8} justifyContent="flex-end" alignItems="flex-end">
+            <VectorImg />
+          </Grid>
+        </Grid>
+      </div>
+  
+    </>
+  );
 }
 
 export default Landing;
