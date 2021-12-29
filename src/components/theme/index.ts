11606e66ec2df97483794945892b0221762e31ec
Akshay Pandey <codeakshay31@gmail.com>
2021-12-29 12:03:45 +0530

    fix: theme setup

--- src/components/theme/index.ts
+++ src/components/theme/index.ts
@@ -1,77 +1,9 @@
-import {
-  ThemeOptions,
-  SimplePaletteColorOptions,
-  Theme,
-  createTheme 
-} from "@mui/material/styles";
-import { blue, orange } from "@mui/material/colors";
-import React from "react";
-export const themeProps: ThemeOptions["props"] = {
-  MuiTextField: {
-    variant: 'outlined',
-    margin: 'dense',
-  },
-};
+import React from 'react';
 
-export const defaultPrimary = blue[500];
-export const defaultSecondary = orange[500];
-export const defaultMode = "light";
-export interface ThemeContext {
-  mode: ThemeOptions["palette"]["props"];
-  primary: SimplePaletteColorOptions["main"];
-  secondary: SimplePaletteColorOptions["main"];
-  toggleMode: () => void;
-  updateColors: (
-    primary?: SimplePaletteColorOptions["main"],
-    secondary?: SimplePaletteColorOptions["main"]
-  ) => void;
-}
-
-export const themeContext = React.createContext<ThemeContext>({
-  mode: "light",
-  primary: blue[500],
-  secondary: blue[500],
-  toggleMode: () => {
-    /* Do nothing */
-  },
-  updateColors: () => {
-    /* Do nothing */
-  },
-});
+export const ThemeContext = React.createContext({ toggleMode: () => {} });
 
 export function toggleMode(
-  setTheme: React.Dispatch<React.SetStateAction<Theme>>
-): void {
-  setTheme((oldTheme) => {
-    return createTheme({
-      // props: themeProps,
-      palette: {
-        primary: {
-          main: oldTheme.palette.primary.main,
-        },
-        secondary: {
-          main: oldTheme.palette.secondary.main,
-        },
-        mode: oldTheme.palette.mode === 'dark' ? 'light' : 'dark',
-      },
-      typography: {
-        fontFamily: [
-          'Nunito',
-          'Montserrat',
-          'Roboto',
-          'sans-serif',
-          'Arial',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-        ].join(','),
-      },
-    });
-  });
-  return;
+  setMode: React.Dispatch<React.SetStateAction<'light' | 'dark'>>
+) {
+  setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
 }
--- src/components/theme/modeToggle.tsx
+++ src/components/theme/modeToggle.tsx
@@ -1,51 +1,53 @@
-import React, { useContext } from "react";
-import { themeContext } from ".";
-import WbSunnyIcon from "@mui/icons-material/WbSunny";
-import Brightness2Icon from "@mui/icons-material/Brightness2";
-import { makeStyles} from "@mui/styles";
-import { Theme } from '@mui/material/styles';
-import clsx from "clsx";
+import React, { useContext } from 'react';
+import { ThemeContext } from '.';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Brightness2Icon from '@mui/icons-material/Brightness2';
+import { makeStyles } from '@mui/styles';
+import { useTheme } from '@mui/material/styles';
+import clsx from 'clsx';
+
 const useStyles = makeStyles(() => ({
-    button: {
-        overflow: "hidden",
-        all: "unset",
-        display: "flex",
-        borderRadius: 50,
-        borderStyle: "solid",
-        cursor: "pointer",
-    },
-    icon: {
-        transform: `translateY(-100%)`,
-        transition: `transform 500ms`,
-        padding: 3,
-    },
-    visibleIcon: {
-        transform: `translateY(0)`,
-    },
+  button: {
+    overflow: 'hidden',
+    all: 'unset',
+    display: 'flex',
+    borderRadius: 50,
+    borderStyle: 'solid',
+    cursor: 'pointer',
+  },
+  icon: {
+    transform: `translateY(-100%)`,
+    transition: `transform 500ms`,
+    padding: 3,
+  },
+  visibleIcon: {
+    transform: `translateY(0)`,
+  },
 }));
 
 const ThemeToggleButton: React.FC = () => {
-    const classes = useStyles();
-    const themeCtx = useContext(themeContext);
-    return (
-        <button className={classes.button} onClick={themeCtx.toggleMode}>
-            <WbSunnyIcon
-                fontSize="small"
-                color="inherit"
-                className={clsx(
-                    classes.icon,
-                    themeCtx.mode === "light" && classes.visibleIcon
-                )}
-            />
-            <Brightness2Icon
-                fontSize="small"
-                color="inherit"
-                className={clsx(
-                    classes.icon,
-                    themeCtx.mode === "dark" && classes.visibleIcon
-                )}
-            />
-        </button>
-    );
+  const classes = useStyles();
+  const theme = useTheme();
+  const themeCtx = useContext(ThemeContext);
+  return (
+    <button className={classes.button} onClick={themeCtx.toggleMode}>
+      <WbSunnyIcon
+        fontSize='small'
+        color='inherit'
+        className={clsx(
+          classes.icon,
+          theme.palette.mode === 'light' && classes.visibleIcon
+        )}
+      />
+      <Brightness2Icon
+        fontSize='small'
+        color='inherit'
+        className={clsx(
+          classes.icon,
+          theme.palette.mode === 'dark' && classes.visibleIcon
+        )}
+      />
+    </button>
+  );
 };
 export default ThemeToggleButton;
