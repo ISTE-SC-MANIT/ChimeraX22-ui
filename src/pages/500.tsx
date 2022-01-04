dcb85311cc31e58c90600831fca43a413f406aa9
22tushar <gehlottushar18@gmal.com>
2022-01-04 15:13:24 +0530

    feat:errorpage

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -5,12 +5,13 @@ import { Typography } from '@mui/material';
 import {Box , useMediaQuery} from '@mui/material'
 import { makeStyles, createStyles } from '@mui/styles';
 import { Theme, useTheme } from '@mui/material/styles';
+import { Link } from '@mui/material';
 import Image from 'next/image'
 const useStyles = makeStyles((theme: Theme) =>
 
   createStyles({
     root: {
-      backgroundColor: '	#00BFFF', 
+      backgroundColor: '#ececec', 
       height: '100vh', 
       padding: '5%',
       display:'flex',
@@ -20,16 +21,16 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 
 );
-export default function ErrorPage() {
+export default function ErrorPage500() {
 
-  const ErrorImg = () => {
+  const ErrorImg500 = () => {
     const theme = useTheme();
     const mobile = useMediaQuery(theme.breakpoints.down('sm'));
     if (mobile) {
       return (
         <Box>
           <Image
-            src='/error.png'
+             src='/500.png'
             alt='logo'
             width={window.innerWidth}
             height={window.innerWidth / 1.74}
@@ -39,7 +40,7 @@ export default function ErrorPage() {
     }
     return (
       <Box>
-        <Image src='/error.png' alt='logo' width={500} height={350} />
+        <Image  src='/500.png' alt='logo' width={450} height={400} />
       </Box>
     );
   };
@@ -49,13 +50,13 @@ export default function ErrorPage() {
     <>
       <div className={classes.root} >
         <CssBaseline />
-            <Typography variant='subtitle1' align='center'>
-              <ErrorImg/>
-              <h2>If you face any difficulty Please contact below email address<br />
-                <a href='#' >istescmanit@gmail.com</a>
-              </h2>
-            </Typography>
-    
+      
+        <Typography variant='subtitle1' align='center'>
+             <ErrorImg500/>
+               Message: "Server Error" <br/><br/>We're sorry , The page you requested could not be found<br/>
+               if you're experiencing a critical issue, please	&nbsp;
+               <Link href="mailto:istescmanit@gmail.com" >email support.</Link> 
+       </Typography>
       </div>
     </>
   );
