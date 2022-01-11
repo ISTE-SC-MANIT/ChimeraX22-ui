09e7254139834f7514441cdf0b1646bb7c8ee246
22tushar <gehlottushar18@gmal.com>
2022-01-11 12:21:42 +0530

    Update:test components

---
+++ src/components/test/questionComponent.tsx
@@ -0,0 +1,354 @@
+import React from 'react';
+import { createStyles, withStyles, WithStyles, makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import MuiDialogTitle from '@mui/material/DialogTitle';
+import MuiDialogContent from '@mui/material/DialogContent';
+import MuiDialogActions from '@mui/material/DialogActions';
+import Image from 'next/image'
+import Audio from "./audioPlayer"
+import Typography from '@mui/material/Typography';
+import { Box, Grid, TextField, Tooltip, Paper, Card, CardActionArea, CardMedia } from '@mui/material';
+// import { GetQuestionsQueryResponse } from '../__generated__/GetQuestionsQuery.graphql';
+import { GetQuestionsQuery } from '../../__generated__/GetQuestionsQuery';
+import { QuestionAnswerType } from '../../__generated__/globalTypes';
+import { Role } from '../../__generated__/globalTypes';
+// import { QuestionAnswer } from '../__generated__/SubmitQuizMutation.graphql';
+// import { Role } from '../__generated__/AppViewerQuery.graphql';
+import VideoPlayer from './videoPlayer';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    minHeight: '100vh',
+  },
+  box: {
+    width: '80%',
+    border: `3px solid ${theme.palette.divider}`,
+  },
+  dialogActions: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  nextBtn: {
+    // marginRight: 'auto',
+    [theme.breakpoints.down('lg')]: {
+      margin: theme.spacing(1),
+    },
+  },
+  reviewBtn: {
+    [theme.breakpoints.down('md')]: {
+      margin: theme.spacing(1),
+    },
+  },
+  noSelect: {
+    userSelect: 'none',
+  },
+  imageBox: {
+    borderStyle: 'solid',
+    borderWidth: theme.spacing(1 / 8),
+    borderRadius: theme.spacing(1 / 4),
+    borderColor: theme.palette.divider,
+    padding: theme.spacing(2),
+    [theme.breakpoints.up('lg')]: {
+      margin: theme.spacing(4),
+    },
+  },
+  videoBox: {
+    [theme.breakpoints.up('lg')]: {
+      margin: theme.spacing(4),
+    },
+  },
+
+  media: {
+    height: 200,
+  },
+}));
+
+
+export interface DialogTitleProps extends WithStyles<typeof styles> {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const DialogTitle = withStyles(styles)((props: DialogTitleProps) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle /*disableTypography*/ className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+interface Props {
+  question: GetQuestionsQueryResponse["getQuestions"][0],
+  answer: QuestionAnswer[],
+  setAnswers: React.Dispatch<React.SetStateAction<QuestionAnswer[] | []>>
+  reviewedAnswers: string[] | [],
+  visitedAnswers: string[] | [],
+  setReviewedAnswers: React.Dispatch<React.SetStateAction<string[] | []>>
+  setVisitedAnswers: React.Dispatch<React.SetStateAction<[] | string[]>>
+  currentQuestion: GetQuestionsQueryResponse["getQuestions"][0]
+  setCurrentQuestion: any
+  questions: GetQuestionsQueryResponse["getQuestions"]
+  role: Role
+
+}
+
+const QuestionComponent: React.FC<Props> = ({
+  question,
+  answer,
+  setAnswers,
+  visitedAnswers,
+  setVisitedAnswers,
+  reviewedAnswers,
+  setReviewedAnswers,
+  currentQuestion,
+  setCurrentQuestion,
+  questions, role
+}) => {
+
+  const getQuestionAnswer = (questionNo: number, answerNo: "ans1" | "ans2") => {
+    console.log(answer)
+    return answerNo === "ans1" ? answer.find((a) => a.questionNumber === questionNo).answer : answer.find((a) => a.questionNumber === questionNo).answer2
+  }
+  const [localState, setLocalState] = React.useState("")
+  const [localState2, setLocalState2] = React.useState("")
+
+  React.useEffect(() => {
+    setLocalState(getQuestionAnswer(question.questionNo, "ans1"))
+    setLocalState2(getQuestionAnswer(question.questionNo, "ans2"))
+    const exists = visitedAnswers.find((answer) => answer === question.id)
+    if (!Boolean(exists)) {
+      setVisitedAnswers([...visitedAnswers, question.id])
+    }
+
+  }, [question])
+
+  const classes = useStyles()
+  const handleClose = () => {
+
+  };
+
+  const handleNext = () => {
+    const index = questions.findIndex((question) => question.id === currentQuestion.id)
+    setCurrentQuestion(questions[index + 1])
+  }
+
+  const handlePrevious = () => {
+    const index = questions.findIndex((question) => question.id === currentQuestion.id)
+    setCurrentQuestion(questions[index - 1])
+  }
+
+
+
+  const saveAnswer = () => {
+    const index = answer.findIndex((answer) => answer.questionNumber === question.questionNo)
+    let answerCopy = [...answer]
+    answerCopy[index].answer = localState
+    answerCopy[index].answer2 = localState2
+    setAnswers(answerCopy)
+
+  }
+
+  const resetAnswer = () => {
+    const index = answer.findIndex((answer) => answer.questionNumber === question.questionNo)
+    let answerCopy = [...answer]
+    answerCopy[index].answer = ""
+    answerCopy[index].answer2 = ""
+    setAnswers(answerCopy)
+    setLocalState("")
+    setLocalState2("")
+  }
+
+  const handleReview = () => {
+    const exists = reviewedAnswers.find((answer) => answer === question.id)
+    if (!Boolean(exists)) {
+      setReviewedAnswers([...reviewedAnswers, question.id])
+    } else {
+      let answerCopy = [...reviewedAnswers]
+      const i = reviewedAnswers.findIndex((answer) => answer === question.id)
+      answerCopy.splice(i, 1)
+      setReviewedAnswers(answerCopy)
+
+    }
+  }
+
+
+  const isMarkedForReview = () => {
+    const exists = reviewedAnswers.find((answer) => answer === question.id)
+    return Boolean(exists)
+  }
+
+
+
+
+  return (
+    <div>
+      <Grid container justifyContent="center" alignItems="center" className={classes.root}>
+        <Box className={classes.box}>
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Question {question.questionNo}
+          </DialogTitle>
+          <DialogContent dividers>
+            <Typography className={classes.noSelect} gutterBottom>
+              {question.question}
+            </Typography>
+            {question.questionType === 'IMAGE' && (
+              <Box className={classes.imageBox}>
+                <Paper elevation={0}>
+                  <Box>
+                    <Image
+                      layout={'responsive'}
+                      height={100}
+                      width={300}
+                      src={question.questionAssets}
+                      alt="logo"
+                    />
+                  </Box>
+                </Paper>
+              </Box>
+            )}
+            {question.questionType === 'AUDIO' && (
+              <Box m={4}>
+                <Audio src={question.questionAssets} />
+              </Box>
+            )}
+            {question.questionType === 'VIDEO' && (
+              <Box className={classes.videoBox}>
+                <VideoPlayer src={question.questionAssets} />
+              </Box>
+            )}
+            <Box>
+              <TextField
+                fullWidth
+                multiline
+                label={question.firstAnswerLabel}
+                onChange={(e) => {
+                  setLocalState(e.target.value);
+                }}
+                value={localState}
+                disabled={role === 'TEAM_HELPER'}
+              />
+            </Box>
+            {question.questionAnswerType === 'DOUBLE' && (
+              <Box>
+                <TextField
+                  fullWidth
+                  multiline
+                  label={question.secondAnswerLabel}
+                  onChange={(e) => {
+                    setLocalState2(e.target.value);
+                  }}
+                  value={localState2}
+                  disabled={role === 'TEAM_HELPER'}
+                />
+              </Box>
+            )}
+          </DialogContent>
+          <DialogActions className={classes.dialogActions}>
+            <Box style={{ marginRight: 'auto' }} className={classes.nextBtn}>
+              <Box>
+                <Button
+                  onClick={handlePrevious}
+                  variant="contained"
+                  color="primary"
+                  disabled={currentQuestion.questionNo === 1}
+                >
+                  Previous
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button
+                  onClick={handleNext}
+                  variant="contained"
+                  color="primary"
+                  disabled={currentQuestion.questionNo === 30}
+                >
+                  Next
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                {role === 'TEAM_LEADER' && (
+                  <Button
+                    onClick={handleReview}
+                    variant="contained"
+                    color="primary"
+                    className={classes.reviewBtn}
+                  >
+                    {isMarkedForReview() ? 'Un-mark for review' : 'mark for review'}
+                  </Button>
+                )}
+              </Box>
+            </Box>
+            {role === 'TEAM_LEADER' && (
+              <Button
+                onClick={resetAnswer}
+                disabled={
+                  !Boolean(
+                    Boolean(
+                      getQuestionAnswer(question.questionNo, 'ans1') ||
+                      Boolean(getQuestionAnswer(question.questionNo, 'ans2'))
+                    )
+                  )
+                }
+                variant="contained"
+                color="primary"
+              >
+                Reset
+              </Button>
+            )}
+            {role === 'TEAM_LEADER' && (
+              <Button
+                onClick={saveAnswer}
+                variant="contained"
+                color="primary"
+                disabled={Boolean(
+                  Boolean(
+                    getQuestionAnswer(question.questionNo, 'ans1') ||
+                    Boolean(getQuestionAnswer(question.questionNo, 'ans2'))
+                  )
+                )}
+              >
+                Save Answer
+              </Button>
+            )}
+          </DialogActions>
+        </Box>
+      </Grid>
+    </div>
+  );
+}
+
+
+export default QuestionComponent
---
+++ src/components/test/quizPage.tsx
@@ -0,0 +1,261 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+// import { useQuery } from "relay-hooks"
+// import query from "../components/relay/queries/GetQuestionsQuery"
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AccessAlarmOutlinedIcon from '@material-ui/icons/AccessAlarmOutlined';
+// import SubmitQuizMutation from "../components/relay/mutations/SubmitQuizMutation"
+import { makeStyles } from '@mui/styles';
+import Timer from './timer';
+import { Divider, ListItemText } from '@mui/material';
+import QuestionPanel from './questionPanel';
+import Stats from './statistics';
+import QuestionComponent from './questionComponent';
+// import { GetQuestionsQuery, GetQuestionsQueryResponse } from "../__generated__/GetQuestionsQuery.graphql"
+// import { QuestionAnswer, SubmitQuizInput } from '../__generated__/SubmitQuizMutation.graphql';
+import { ComponentProps } from '../../pages/_app';
+import LoadingScreen from '../loadingScreen';
+// import timeQuery from "../components/relay/queries/GetQuizStartTimeQuery"
+// import { GetQuizStartTimeQuery } from '../__generated__/GetQuizStartTimeQuery.graphql';
+import { useRouter } from 'next/router';
+import SubmitQuizBox from './submitquiz'
+import CustomDrawer from '../navbar/customDrawer';
+import Navbar from '../navbar/Navbar';
+import InstructionsDialog from './InstructionsDialog';
+import { Theme } from '@mui/material/styles'
+import { GetQuestionsQuery } from '../../__generated__/GetQuestionsQuery';
+import { GetQuizStartTimeQuery } from '../../__generated__/GetQuizStartTimeQuery';
+
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    minHeight: '90vh',
+  },
+  paper: {
+    margin: theme.spacing(2, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  divider: {
+    width: "100%",
+    margin: theme.spacing(2, 0)
+  },
+  insBtn: {
+    marginBottom: theme.spacing(1)
+  }
+}));
+
+interface QuizPageProps extends ComponentProps {
+  setQuizStatus: () => void
+}
+
+
+
+
+const QuizPage: React.FC<QuizPageProps> = ({
+  viewer,
+  setSuccessMessage,
+  setErrorMessage,
+  setQuizStatus,
+  refetch,
+}) => {
+  const classes = useStyles();
+  const { data, error, retry, isLoading } = useQuery<GetQuestionsQuery>(query);
+  const {
+    data: startTimeData,
+    error: startTimeError,
+    retry: startTimeRetry,
+    isLoading: startTimeIsLoading,
+  } = useQuery<GetQuizStartTimeQuery>(timeQuery);
+  const [currentQuestion, setCurrentQuestion] = React.useState<
+    GetQuestionsQueryResponse['getQuestions'][0] | null
+  >(null);
+  const [answer, setAnswer] = React.useState<QuestionAnswer[] | []>([]);
+  const [reviewedAnswers, setReviewedAnswers] = React.useState<string[] | []>([]);
+  const [visitedAnswers, setVisitedAnswers] = React.useState<string[] | []>([]);
+  const [submit, setSubmit] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const router = useRouter();
+
+  const handleClose = () => setSubmit(false);
+
+  React.useEffect(() => {
+    setCurrentQuestion(data?.getQuestions[0]);
+    if (Boolean(data)) {
+      const answerMap: QuestionAnswer[] = data.getQuestions.map((question) => {
+        return {
+          answer: '',
+          answer2: '',
+          questionId: question.id,
+          questionNumber: question.questionNo,
+        };
+      });
+      setAnswer(answerMap);
+    }
+  }, [data]);
+
+  if (isLoading || startTimeIsLoading) {
+    return <LoadingScreen loading />;
+  }
+
+  const handleQuestionClick = (questionNo: number) => {
+    const clickedQuestion = data.getQuestions.find((ques) => ques.questionNo === questionNo);
+    setCurrentQuestion(clickedQuestion);
+  };
+
+  const handleSubmitQuizMutation = () => {
+    const input: SubmitQuizInput = { responses: answer };
+    SubmitQuizMutation(input, {
+      onCompleted: () => {
+        setSuccessMessage('Quiz was successfully Submitted');
+        setQuizStatus();
+      },
+      onError: () => {
+        setErrorMessage('Something went wrong');
+        setQuizStatus();
+      },
+    });
+  };
+
+  return (
+    <>
+      <InstructionsDialog
+        viewer={viewer}
+        openDialog={openDialog}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+        refetch={refetch}
+        onClose={() => setOpenDialog(false)}
+      />
+      <CustomDrawer
+        name={viewer.name}
+        username={viewer.email}
+        open={open}
+        setOpen={setOpen}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+      />
+      <Navbar
+        setOpen={setOpen}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+      />
+      <SubmitQuizBox
+        viewer={viewer}
+        submit={submit}
+        handleClose={handleClose}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+        refetch={refetch}
+        setQuizStatus={setQuizStatus}
+        answer={answer}
+      />
+      <div className={classes.root} onClick={() => setOpen(false)}>
+        <Grid container component="main" className={classes.root}>
+          <CssBaseline />
+          <Grid item xs={12} md={6} lg={8} style={{ position: 'relative' }}>
+            {currentQuestion ? (
+              <QuestionComponent
+                question={currentQuestion}
+                answer={answer}
+                setAnswers={setAnswer}
+                reviewedAnswers={reviewedAnswers}
+                setReviewedAnswers={setReviewedAnswers}
+                visitedAnswers={visitedAnswers}
+                setVisitedAnswers={setVisitedAnswers}
+                currentQuestion={currentQuestion}
+                setCurrentQuestion={setCurrentQuestion}
+                questions={data.getQuestions}
+                role={viewer.role}
+              />
+            ) : (
+              <LoadingScreen loading />
+            )}
+            <Grid
+              container
+              item
+              xs={12}
+              justifyContent="center"
+              alignItems="center"
+              className={classes.insBtn}
+            >
+              <Button variant="contained" color="primary" onClick={() => setOpenDialog(true)}>
+                Instructions
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4} component={Paper} elevation={6} square>
+            <div className={classes.paper}>
+              <Box display="flex">
+                <Avatar className={classes.avatar}>
+                  <AccessAlarmOutlinedIcon />
+                </Avatar>
+                <Box mt={1}>
+                  <Timer startTime={startTimeData} onTimeUp={handleSubmitQuizMutation} />
+                </Box>
+              </Box>
+              <Divider variant="middle" className={classes.divider} />
+              <QuestionPanel
+                questions={data.getQuestions}
+                onQuestionClick={handleQuestionClick}
+                currentQuestion={currentQuestion}
+                reviewedAnswers={reviewedAnswers}
+                answers={answer}
+                visitedAnswers={visitedAnswers}
+              />
+              <Divider variant="middle" className={classes.divider} />
+              {viewer.role === 'TEAM_LEADER' && (
+                <>
+                  {' '}
+                  <Stats
+                    reviewedAnswers={reviewedAnswers}
+                    answers={answer}
+                    visitedAnswers={visitedAnswers}
+                  />
+                  <Divider variant="middle" className={classes.divider} />
+                  <Box mb={2} width="100%">
+                    <ListItemText
+                      primary={'Submit Quiz'}
+                      secondary={'Quiz will be submitted automatically when time is over'}
+                      primaryTypographyProps={{ variant: 'h6' }}
+                    />
+                  </Box>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    // onClick={() => handleSubmitQuizMutation()}
+                    onClick={() => {
+                      setSubmit(true);
+                    }}
+                  >
+                    SUBMIT
+                  </Button>
+                </>
+              )}
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
+
+export default QuizPage
