d348e2c4badc94bdbe07c0f1feb787fce330f61c
shubh asati <shubhtrikal9876@gmail.com>
2022-02-16 15:45:12 +0530

    fix:btn

--- src/pages/dashboard/test.tsx
+++ src/pages/dashboard/test.tsx
@@ -120,7 +120,7 @@ const Test: React.FC<ComponentProps> = ({
         <Box marginBottom={4}>
           <Button
             onClick={handleStartQuiz}
-            disabled={disableButton}
+            // disabled={disableButton}
             variant='contained'
             color='primary'
           >
